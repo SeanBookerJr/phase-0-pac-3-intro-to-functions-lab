8ea0a7bbe5919b9d77afb54760a073796c8dd769
Sean Booker Jr <seanbookerjunior@gmail.com>
2022-04-29 01:00:52 -0400

    commit tree 289f545481ce302f7cc52562c778eaf63c45e1db

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+
+function whisper(string) {
+    return string.toLowerCase()
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase("hello"))
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase("hello"))
+}
+
+function sayHiToHeadphonedRoommate(string)
+
+{   return "I can't hear you!"
+} 
